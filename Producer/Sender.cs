31911c5a42d4153b221dc605380100f97a50d452
Nishant Kumar <nkumar1@galaxe.com>
2019-11-15 21:45:18 +0530

    Added project

---
+++ Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Sender
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicTest", false, false, false, null);
+                    string message = "getting started with dot net core RabbitMQ";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", "BasicTest",null, body);
+                    Console.WriteLine("Sent Message {0}", message);
+                }
+
+                Console.WriteLine("Press [enter] to exit the sender app");
+                Console.ReadLine();
+            }
+        }
+    }
+}
